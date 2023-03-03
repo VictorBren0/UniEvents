7055645e201bdb52f580e522160a453931f344b0
VictorBren0 <victor.bsr2000@gmail.com>
2023-03-03 11:05:21 -0300

    FIX: Correção

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import Route from './src/routes/routes';
+import Route from './src/routes/Routes';
 
 export default function App(){
   return(
---
+++ src/services/api.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+
+const api = axios.create({
+  baseURL: `http://192.168.100.8:3000/`,
+});
+
+export const getMaps = async () => {
+  return api.get(
+    `/maps`,
+  ).catch(err => {
+    console.log(err);
+  });
+};
+
+  export const getCategorys = async () => {
+    return api.get(
+      `/categorys`,
+    ).catch(err => {
+      console.log(err);
+    });
+};
+
+
+
+export default api;
